a27441be0ab412900fdb95b26213f2150adedbe7
banerjri <banerjri@mail.uc.edu>
2023-06-16 09:53:11 -0500

    done

---
+++ src/VAriable.java
@@ -0,0 +1,24 @@
+public class VAriable
+{
+    private static boolean yes;
+
+    public static void main(String[] args)
+    {
+        int numberOfKids = 3;
+        boolean isItRaining = false;
+        double gasPrice = 3.5;
+        int favNumber = 6;
+        double shoeSize = 7.5;
+        int birthMonth = 5;
+        String Name = "Ria";
+        System.out.println("The Number of kids in your family " + numberOfKids );
+        System.out.println("The answer to isit raining " + isItRaining);
+        System.out.println("the price of a gallon of gas" + gasPrice);
+        System.out.println("Your Favorite number" + favNumber);
+        System.out.println("Your Shoe Size " + shoeSize);
+        System.out.println("Your birth Month " + birthMonth);
+        System.out.println("Your full name " + Name);
+    }
+
+
+}
